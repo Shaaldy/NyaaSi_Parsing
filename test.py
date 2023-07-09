a6495ff90e65dc8af8b1ff2f251bc5d39e194086
Shaaldy <138975611+Shaaldy@users.noreply.github.com>
2023-07-09 16:19:26 +0500

    test.py

--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-import re
+from NyaaSiWriter import writer
+from parsNyaaSi import array
 
-match = "Some text [1080p] some more text"
 
-match_q = re.search(r'\[(\d+p)\]', match)
-if match_q:
-    quality = match_q.group(1)
-    print("Quality: " + quality)
-
-match_q = re.search(r'\((\d+p)\)', match)
-if match_q:
-    quality = match_q.group(1)
-    print("Quality: " + quality)
+writer(array)
